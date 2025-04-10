caadc136a86e9e81f9b960e828c0021b17d7689e
TomCab98 <tomy.cananillas@gmail.com>
2025-04-10 13:39:21 -0300

    style: add delete animation for tasks

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private cookieService: CookieService) {}
 
   canActivate(): boolean {
-    const isLoggedIn = !!localStorage.getItem('token');
+    const isLoggedIn = !!this.cookieService.get('token');
 
     if (!isLoggedIn) {
       this.router.navigate(['/login']);
